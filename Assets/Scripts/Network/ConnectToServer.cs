179dee1fab5c67e40200660b63fa210c2ef6871a
Evgeny Zimin <94135768+iamzimin@users.noreply.github.com>
2023-02-25 14:37:01 +0400

    works?

--- Assets/Scripts/Network/ConnectToServer.cs
+++ Assets/Scripts/Network/ConnectToServer.cs
@@ -7,15 +7,35 @@ using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    public string region;
     public string mainSceneName;
 
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectToRegion(region);
     }
 
     public override void OnConnectedToMaster()
     {
+        Debug.Log("Вы подключилиь к: " + PhotonNetwork.CloudRegion);
+        //PhotonNetwork.JoinLobby();
+
         SceneManager.LoadScene(mainSceneName);
+
     }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //base.OnDisconnected(cause);
+        Debug.Log("Вы отключены от сервера");
+        Debug.Log(cause);
+    }
+
+    //public override void OnJoinedLobby()
+    //{
+    //    RoomOptions roomOptions = new RoomOptions();
+    //    roomOptions.MaxPlayers = 4;
+    //    PhotonNetwork.CreateRoom("qwe", roomOptions, TypedLobby.Default);
+    //}
 }
--- Assets/Scripts/Network/CreateLobby.cs
+++ Assets/Scripts/Network/CreateLobby.cs
@@ -24,55 +24,65 @@ public class CreateLobby : MonoBehaviourPunCallbacks
 
     public void CreateRoom()
     {
+        if (!PhotonNetwork.IsConnected)
+        {
+            Debug.Log("Вы не можете создать лобби так как не подключены к серверу");
+            return;
+        }
+
         System.Random rnd = new System.Random();
         int code = rnd.Next(100000, 999999);
 
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 4;
-        roomOptions.EmptyRoomTtl = 60000;
-        roomOptions.PlayerTtl = 60000;
-        roomOptions.IsOpen = true;
-        roomOptions.IsVisible = true;
+        //roomOptions.EmptyRoomTtl = 60000;
+        //roomOptions.PlayerTtl = 60000;
+        //roomOptions.IsOpen = true;
+        //roomOptions.IsVisible = true;
 
-        lobbyName = lobbyNameTMP.text;
-        lobbyPassword = lobbyPasswordTMP.text;
-        lobbyIconID = iconScroller.selectedId.ToString();
+        //lobbyName = lobbyNameTMP.text;
+        //lobbyPassword = lobbyPasswordTMP.text;
+        //lobbyIconID = iconScroller.selectedId.ToString();
 
-        roomOptions.CustomRoomPropertiesForLobby = new string[] { "lobbyName", "lobbyPassword", "lobbyIconID", "lobbyCode" };
-        roomOptions.CustomRoomProperties = new Hashtable
-        { { "lobbyName", lobbyName }, { "lobbyPassword", lobbyPassword }, {"lobbyIconID", lobbyIconID }, {"lobbyCode", "123"} };
+        //roomOptions.CustomRoomPropertiesForLobby = new string[] { "lobbyName", "lobbyPassword", "lobbyIconID", "lobbyCode" };
+        //roomOptions.CustomRoomProperties = new Hashtable
+        //{ { "lobbyName", lobbyName }, { "lobbyPassword", lobbyPassword }, {"lobbyIconID", lobbyIconID }, {"lobbyCode", "qwe"} };
 
 
 
-        PhotonNetwork.CreateRoom("123", roomOptions, TypedLobby.Default, null);
+        PhotonNetwork.CreateRoom(lobbyNameTMP.text, roomOptions, TypedLobby.Default);
         
 
-        Debug.Log(code);
+        //Debug.Log(code);
 
     }
 
     public override void OnCreatedRoom()
     {
-        Debug.Log(PhotonNetwork.CountOfRooms.ToString());
-        Debug.Log(PhotonNetwork.CurrentRoom.Name);
+        //Debug.Log(PhotonNetwork.CountOfRooms.ToString());
+        //Debug.Log(PhotonNetwork.CurrentRoom.Name);
         PhotonNetwork.LoadLevel(lobbbySceneName);
+        Debug.Log("Количество комнат: " + PhotonNetwork.CountOfRooms.ToString());
+        Debug.Log("Создана комната: " + PhotonNetwork.CurrentRoom.Name);
+        //PhotonNetwork.JoinRoom(lobbyNameTMP.text);
+        Debug.Log("Количество комнат: " + PhotonNetwork.CountOfRooms.ToString());
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         base.OnCreateRoomFailed(returnCode, message);
+        Debug.Log("Ошибка создания лобби");
         Debug.Log(returnCode);
         Debug.Log(message);
     }
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        Debug.Log("ssss");
+        Debug.Log("Попал в функцию OnRoomListUpdate");
         foreach (RoomInfo room in roomList)
         {
-            Debug.Log(room.Name);
+            Debug.Log("Сущетвует комната: " + room.Name);
         }
-        Debug.Log("ssss");
 
     }
 
