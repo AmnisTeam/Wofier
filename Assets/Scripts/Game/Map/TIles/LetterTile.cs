04efb787d4ab72395a05a49df216cc6c1ae1cf98
KoTeuKaSeeker <emaillit8@gmail.com>
2023-03-07 21:28:10 +0400

    fix: fix bag with color

--- Assets/Scripts/Game/Map/TIles/LetterTile.cs
+++ Assets/Scripts/Game/Map/TIles/LetterTile.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using static Photon.Pun.Demo.Shared.DocLinks;
 
 public class LetterTile : Tile
